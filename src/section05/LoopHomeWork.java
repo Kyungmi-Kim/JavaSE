dbb4fc8bd2df5d2a70fd9b224e0f132dfd67fb40
saint <saint@VIVO>
2023-07-26 19:36:18 +0900

    20230724

--- src/section05/LoopHomeWork.java
+++ src/section05/LoopHomeWork.java
@@ -34,14 +34,34 @@ package section05;
   ***		 
    *		
  
-5번 구구단
-	 2 x 1 = 2 	3 x 1 = 3	4 x 1 = 4
-	 
-	 
-
-
 
  */
 public class LoopHomeWork {
-
+	public static void main(String[] args) {
+		
+		for(int i = 0; i < 7; i++) {
+			for(int j = 0; j < 7; j++) {
+				if(i < 7/2 +1) {
+					
+					if(j < 3 - i) {
+						System.out.print(" ");
+				} 	else if (j > i + 3) {
+					    System.out.print(" ");
+				}   else {
+						System.out.print("*");
+				}
+			}   else {
+					if (j < i - 3) {
+						System.out.print(" ");
+					} else if (j > 9 - i) {
+						System.out.print(" ");
+					}else {
+						System.out.print("*");
+					}
+				}
+			}
+			System.out.println(); // 개행
+		}
+		
+	}
 }
